9780b10660c69da2f18d8746e711017bf175dbf7
n00bcybot <freshmaker21@gmail.com>
2023-09-23 19:52:18 +0200

    Testing

--- test.py
+++ test.py
@@ -1,15 +1,5 @@
-import maya.cmds as cmds
-import maya.standalone
+import subprocess, sys
 
+p = subprocess.Popen(["powershell.exe", "(Get-Process mayapy).HasExited"], stdout=sys.stdout)
+p.communicate()
 
-maya.standalone.initialize()
-
-file_to_render = "%USERPROFILE%\\Desktop\\maya\\rendertest\\test.ma"     # "Browse" button
-cmds.file(file_to_render, o = True)   # "Browse button"
-
-import mtoa.utils as mutils
-mutils.createLocator("aiSkyDomeLight", asLight=True)
-
-cmds.file(s = True)   # "Browse button"
-
-maya.standalone.uninitialize()
